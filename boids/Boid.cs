f592b5df3dbfcb5e8dd5e2e1b3a0ebd2fc190761
lukxstib <stiborlukas@gmail.com>
2025-08-08 12:25:58 +0200

    boids.cs

---
+++ boids/Boid.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+
+namespace boids
+{
+    public class Boid
+    {
+        public Vector Position { get; set; }
+        public Vector Velocity { get; set; }
+        public double MaxSpeed { get; set; } = 2.0;
+        public double PerceptionRadius { get; set; } = 50.0; 
+
+        public Boid(double x, double y)
+        {
+            Position = new Vector(x, y);
+            Random rand = new Random();
+        }
+
+        public void Update(Boid[] boids, double separationStrength, double alignmentStrength, double cohesionStrength, double canvasWidth, double canvasHeight)
+        {
+            Vector separation = CalculateSeparation(boids);
+            Vector alignment = CalculateAlignment(boids);
+            Vector cohesion = CalculateCohesion(boids);
+        }
+
+
+        private Vector CalculateSeparation(Boid[] boids)
+        {
+            
+        }
+
+        private Vector CalculateAlignment(Boid[] boids)
+        {
+            
+        }
+
+        private Vector CalculateCohesion(Boid[] boids)
+        {
+            
+        }
+    }
+}
