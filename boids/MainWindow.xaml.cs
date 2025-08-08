13d099f5e7b1aa479b400b2b77407f8c26f7e21a
lukxstib <stiborlukas@gmail.com>
2025-08-08 14:16:11 +0200

    zakladni funkce a zobrazovani boidu

--- boids/MainWindow.xaml.cs
+++ boids/MainWindow.xaml.cs
@@ -11,24 +11,106 @@ namespace boids
 {
     public partial class MainWindow : Window
     {
+        private List<Boid> boids = new List<Boid>();
+        private DispatcherTimer timer;
+        private bool isRunning = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            // timer pro imulaci Update()
+            timer = new DispatcherTimer();
+            
+            // ~60 FPS
+            timer.Interval = TimeSpan.FromMilliseconds(16);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // update boids
+            double separationStrength = SeparationSlider.Value;
+            double alignmentStrength = AlignmentSlider.Value;
+            double cohesionStrength = CohesionSlider.Value;
+            double canvasWidth = SimulationCanvas.ActualWidth;
+            double canvasHeight = SimulationCanvas.ActualHeight;
+
+            foreach (var boid in boids)
+            {
+                boid.Update(boids.ToArray(), separationStrength, alignmentStrength, cohesionStrength, canvasWidth, canvasHeight);
+            }
+
+            // render boids
+            RenderBoids();
+        }
+
+        private void RenderBoids()
+        {
+            SimulationCanvas.Children.Clear();
+            foreach (var boid in boids)
+            {
+                // tvar boidu
+                Polygon triangle = new Polygon
+                {
+                    Fill = Brushes.Blue,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 1,
+                    Points = new PointCollection
+                    {
+                        new Point(0, 0),
+                        new Point(-5, 2.5),
+                        new Point(-5, -2.5)
+                    }
+                };
+
+                double angle = Math.Atan2(boid.Velocity.Y, boid.Velocity.X) * 180 / Math.PI;
+
+                triangle.RenderTransform = new TransformGroup
+                {
+                    Children = new TransformCollection
+                    {
+                        new RotateTransform(angle),
+                        new TranslateTransform(boid.Position.X, boid.Position.Y)
+                    }
+                };
+
+                SimulationCanvas.Children.Add(triangle);
+
+            }
         }
 
         private void StartButton_Click(object sender, RoutedEventArgs e)
         {
-
+            if (!isRunning)
+            {
+                if (boids.Count == 0)
+                {
+                    Random rand = new Random();
+                    int boidCount = (int)BoidCountSlider.Value;
+                    for (int i = 0; i < boidCount; i++)
+                    {
+                        double x = rand.NextDouble() * SimulationCanvas.ActualWidth;
+                        double y = rand.NextDouble() * SimulationCanvas.ActualHeight;
+                        boids.Add(new Boid(x, y));
+                    }
+                }
+                timer.Start();
+                isRunning = true;
+            }
         }
 
         private void PauseButton_Click(object sender, RoutedEventArgs e)
         {
-
+            timer.Stop();
+            isRunning = false;
         }
 
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
-
+            timer.Stop();
+            isRunning = false;
+            boids.Clear();
+            SimulationCanvas.Children.Clear();
         }
 
         private void ImportButton_Click(object sender, RoutedEventArgs e)
@@ -38,7 +120,7 @@ namespace boids
 
         private void ExportButton_Click(object sender, RoutedEventArgs e)
         {
-            
+
         }
     }
 }
