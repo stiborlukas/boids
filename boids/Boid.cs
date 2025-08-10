cf53111cac977bdc011e2be6ee5bfefc4cc4f13d
Lukáš Stibor <stiborlukas@gmail.com>
2025-08-10 18:20:41 +0200

    update

--- boids/Boid.cs
+++ boids/Boid.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace boids
@@ -8,12 +9,18 @@ namespace boids
         public Vector Position { get; set; }
         public Vector Velocity { get; set; }
         public double MaxSpeed { get; set; } = 2.0;
-        public double PerceptionRadius { get; set; } = 50.0; 
+        public double PerceptionRadius { get; set; } = 50.0;
+
+        // trail
+        public Queue<Vector> Trail { get; } = new Queue<Vector>();
+        public int MaxTrailLength { get; set; } = 15;
 
         public Boid(double x, double y)
         {
             Position = new Vector(x, y);
             Random rand = new Random();
+
+            Velocity = new Vector(rand.NextDouble() * 2 - 1, rand.NextDouble() * 2 - 1) * MaxSpeed;
         }
 
         public void Update(Boid[] boids, double separationStrength, double alignmentStrength, double cohesionStrength, double canvasWidth, double canvasHeight)
@@ -21,6 +28,26 @@ namespace boids
             Vector separation = CalculateSeparation(boids);
             Vector alignment = CalculateAlignment(boids);
             Vector cohesion = CalculateCohesion(boids);
+
+            Vector acceleration = separation * separationStrength + alignment * alignmentStrength + cohesion * cohesionStrength;
+
+            Velocity += acceleration;
+            if (Velocity.Length > MaxSpeed)
+            {
+                Velocity = Velocity / Velocity.Length * MaxSpeed;
+            }
+
+            Position += Velocity;
+
+            if (Position.X < 0) Position = new Vector(Position.X + canvasWidth, Position.Y);
+            if (Position.X > canvasWidth) Position = new Vector(Position.X - canvasWidth, Position.Y);
+            if (Position.Y < 0) Position = new Vector(Position.X, Position.Y + canvasHeight);
+            if (Position.Y > canvasHeight) Position = new Vector(Position.X, Position.Y - canvasHeight);
+
+            // trail
+            Trail.Enqueue(Position);
+            if (Trail.Count > MaxTrailLength)
+                Trail.Dequeue();
         }
 
 
